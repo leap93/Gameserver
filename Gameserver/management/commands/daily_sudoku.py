d5a9de93ce17624d34e3b77ec7b93541a97766fc
Lea Pälkiö <lea-palkio@outlook.com>
2025-10-03 08:46:56 +0300

    time fix

--- Gameserver/management/commands/daily_sudoku.py
+++ Gameserver/management/commands/daily_sudoku.py
@@ -1,8 +1,8 @@
 import json
-from datetime import datetime, date, time
-from Gameserver.views.sudoku_view import random_sudoku, solve_recursive_up
+from datetime import datetime, date
+from Gameserver.views.sudoku_view import random_sudoku, solve_recursive_up, copy_sudoku
 from app.models import DailyPuzzle
-
+import time
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         daily = DailyPuzzle.objects.filter(type="sudoku", created_at__date=date(now.year, now.month, now.day))
         if len(daily) == 0:
             sudoku = random_sudoku()
-            DailyPuzzle.objects.create(type="sudoku", puzzle_text=json.dumps(sudoku), solution=json.dumps(solve_recursive_up(sudoku, -1, 0, time.time() + 10)))
+            DailyPuzzle.objects.create(type="sudoku", puzzle_text=json.dumps(sudoku), solution=json.dumps(solve_recursive_up(sudoku, copy_sudoku(sudoku), -1, 0, time.time() + 10)))
             self.stdout.write(self.style.SUCCESS('Sudoku created!'))
         else:
             self.stdout.write(self.style.SUCCESS('Sudoku already exists!'))

1c0f594420c8a8907bd518d22aa728f7d7c38469
Lea Pälkiö <lea-palkio@outlook.com>
2025-10-05 18:19:34 +0300

    Another sudoku bug fix

--- Gameserver/views/wordle_view.py
+++ Gameserver/views/wordle_view.py
@@ -13,11 +13,11 @@ def wordle_view(request):
     daily = DailyPuzzle.objects.filter(type="wordle", created_at__date=date(now.year, now.month, now.day))
     context = {'translations': get_translations(language), "language": language, "solved": 0}
 
-    with open('app/static/sanat.csv', newline='') as f:
+    with open('app/static/sanat.csv', newline='', encoding="utf-8") as f:
         reader = csv.reader(f)
         data = list(reader)
-        word = random.choice(data)[0]
+        data = [part[0].replace("Ã", "ä").replace("Ã", "ö") for part in data]
+        word = random.choice(data)
         context['word'] = word
         context['words'] = data
-
     return render(request, 'wordle.html', context)

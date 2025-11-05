59a29a8eca45668ce832123a137e3a97b022bc49
Lea Pälkiö <lea-palkio@outlook.com>
2025-11-05 19:13:58 +0200

    Moving login page to "root"

--- Gameserver/urls.py
+++ Gameserver/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from Gameserver.views import login_view, register_view, front_page_view, sudoku_view, logout_view, wordle_view
 
 urlpatterns = [
-    path('login/', login_view.login_view, name='login'),
+    path('/', login_view.login_view, name='login'),
     path('register/', register_view.register_view, name='register'),
     path('front_page/', front_page_view.front_page_view, name='front_page'),
     path('sudoku/', sudoku_view.sudoku_view, name='sudoku'),
--- Gameserver/views/logout_view.py
+++ Gameserver/views/logout_view.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import logout
 from app.utils import get_translations
 
+@login_required
 def logout_view(request):
-    language = request.GET.get('language')
+    language = request.user.userinfo.language
     logout(request)
     translations = get_translations(language)
     context = {'translations': translations, 'language': language}
